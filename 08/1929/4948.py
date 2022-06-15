bd1b58a012d1be333fc017ac99ff47512bb23ab9
LEEYONGHEE1 <lyh990104@naver.com>
2022-06-15 22:00:07 +0900

    시간 복잡도 작업중

--- 08/1929/4948.py
+++ 08/1929/4948.py
@@ -1,28 +1,24 @@
+from cgitb import reset
 from cmath import sqrt
 
 def isprime(num):
-    sqr = int(num**(1/2))+1
-    if num == 1:
-        return False
-    for j in range(2,sqr):
-        if num%j == 0:
-            return False
-    return True
-
-plate = []
-count = 0
-
+ 
+    for i in range(num+1 , num*2+1):
+        result = []
+        sqr = int(i**(1/2))+1
+        if num == 1:
+            pass
+        for j in range(2,sqr):
+            if i%j == 0:
+                pass
+        result.append(i)
+    return len(result)
+    
 while(1):
     user_input = int(input())
     if(user_input == 0):
         break
     else:
-        for k in range(user_input+1, 2*user_input+1):
-            if isprime(k):
-                count += 1
-        plate.append(count)
-        count = 0
-
-for i in plate:
-    print(i)
-    
+        a = isprime(user_input)
+        if isprime:
+            print(a)
