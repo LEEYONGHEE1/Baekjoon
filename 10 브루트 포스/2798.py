cd141388b90cff6ffc844766450e9a832b3465e0
LEEYONGHEE1 <lyh990104@naver.com>
2022-07-07 22:04:05 +0900

    블랙잭

---
+++ 10 브루트 포스/2798.py
@@ -0,0 +1,15 @@
+hap = 0
+hap_number = 0
+
+count, max_number = map(int, input().split())
+
+number = list(map(int, input().split()))
+
+for i in range(count):
+    for j in range(i+1,count):
+        for k in range(j+1, count):
+            hap = number[i] + number[j] + number[k]
+            if hap <= max_number:
+                hap_number = max(hap, hap_number)
+
+print(hap_number)
