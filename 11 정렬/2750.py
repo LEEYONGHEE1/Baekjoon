cc2bd3689ba32ce83e550a574580cbf8930dbd8b
LEEYONGHEE1 <lyh990104@naver.com>
2022-07-12 18:21:00 +0900

    2750

--- 11 정렬/2750.py
+++ 11 정렬/2750.py
@@ -2,4 +2,4 @@ import sys
 array=[int(sys.stdin.readline().rstrip()) for i in range(int(input()))]
 array.sort()
 for i in array:
-    sys.stdout.write(str(i)+'\n') 
+    sys.stdout.write(str(i)+'\n')  
