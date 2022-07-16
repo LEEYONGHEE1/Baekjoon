04c2e8336c3aca695fd4e27848e8a83832f9c3c1
LEEYONGHEE1 <lyh990104@naver.com>
2022-07-16 15:27:45 +0900

    통계학

--- 11 정렬/2108.py
+++ 11 정렬/2108.py
@@ -1,20 +1,19 @@
+import sys
 from collections import Counter
 
-a = int(input()) # a는 홀수다 
+a = int(sys.stdin.readline()) # a는 홀수다 
 
 b = []
 sum1 = 0
 result = 0
 
 for i in range(a):
-    c = int(input())
-    b.append(c)
-    sum1 += c
+    b.append(int(sys.stdin.readline()))
 
 b.sort()
 
 # 산술 평균
-result = round(sum1 / a)
+result = round(sum(b) / a)
 print(result)
 # 중앙값
 middle = b[int(a/2)]
@@ -24,11 +23,11 @@ if(a == 1):
     print(0)
 else:
     #최빈값
-    counter = Counter(b)
-    print(counter)
-    print(f"{counter.most_common(1)} asdas")
-    
-    print(b[1])
+    counter = Counter(b).most_common()
+    if(counter[0][1] == counter[1][1]):
+        print(counter[1][0])
+    else:
+        print(counter[0][0])
     #범위
     print(abs(b[-1] - b[0]))
 
