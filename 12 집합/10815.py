cc4d58d4bf1027a6b466e2b839a0fcefac3d2cbf
LEEYONGHEE1 <lyh990104@naver.com>
2022-07-27 22:31:22 +0900

    숫자 카드

--- 12 집합/10815.py
+++ 12 집합/10815.py
@@ -1,14 +1,18 @@
+import sys
+input = sys.stdin.readline
+
 a1 = int(input())
 a2 = list(map(int, input().split()))
+
+dict = {t: 1 for t in a2}
 
 b1 = int(input())
 b2 = list(map(int, input().split()))
 
-for i in range(len(b2)):
-    if(b2[i] in a2):
-        b2[i] = 1
-    else:
-        b2[i] = 0
-
 for i in b2:
-    print(i, end=' ')
+    if dict.get(i):
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
+
+
