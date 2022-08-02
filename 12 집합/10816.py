1b5251c85d36c8d1e7812038d710a7de4688f672
LEEYONGHEE1 <lyh990104@naver.com>
2022-08-02 20:22:31 +0900

    숫자 카드 2

--- 12 집합/10816.py
+++ 12 집합/10816.py
@@ -1,14 +1,19 @@
 import sys
+from typing import Counter
 input = sys.stdin.readline
 
 count = 0
 
 a1 = int(input())
-a2 = list(map(int, input().split()))
-
+a2 = sorted(list(map(int, input().split())))
 
 b1 = int(input())
 b2 = list(map(int, input().split()))
 
-for i in b2:
-    print(a2.count(i), end=' ')
+counter = Counter(a2)
+
+for i in range(len(b2)):
+    if b2[i] in counter:
+        print(counter[b2[i]], end = ' ')
+    else:
+        print(0, end=' ')
