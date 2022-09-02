17eaf114e334aca82b944f08cf3c7428a58b51e5
LEEYONGHEE1 <lyh990104@naver.com>
2022-09-02 22:40:49 +0900

    스택

--- 19 스택/10828.py
+++ 19 스택/10828.py
@@ -1,25 +1,27 @@
+import sys
+
 a = int(input())
 
 stack = []
 
 for i in range(a):
-    b = input()
-    if(b[:2] == "pu"):
-        stack.append(b[5:])
-    elif(b == "pop"):
+    b = sys.stdin.readline().split()
+    if(b[0] == "push"):
+        stack.append(b[1])
+    elif(b[0] == "pop"):
         if(len(stack) == 0):
             print(-1)
         else:
             print(stack[-1])
             stack.pop()
-    elif(b == "size"):
+    elif(b[0] == "size"):
         print(len(stack))
-    elif(b == "empty"):
+    elif(b[0] == "empty"):
         if(len(stack) == 0):
             print(1)
         else:
             print(0)
-    elif(b =="top"):
+    elif(b[0] == "top"):
         if(len(stack) == 0):
             print(-1)
         else:
