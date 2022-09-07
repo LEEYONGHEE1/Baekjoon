6f77aa9fa3df2b0c0acd04d50189dbe76348308b
LEEYONGHEE1 <lyh990104@naver.com>
2022-09-07 22:21:00 +0900

    스택 수열

---
+++ 19 스택/1874.py
@@ -0,0 +1,30 @@
+n = int(input())
+
+su = []
+stack = []
+state = True
+
+count = 1
+
+for _ in range(n):
+    num = int(input())
+    while count <= num:
+        stack.append(count)
+        su.append("+")
+        count += 1
+    if(stack[-1] == num):
+        stack.pop()
+        su.append("-")
+    else:
+        state = False
+        
+if not state:
+    print("NO")
+else:
+    for i in su:
+        print(i)
+
+
+    
+
+
