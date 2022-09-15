00c6cf06db0098feffdd11c3e2c43d26dacac24d
LEEYONGHEE1 <lyh990104@naver.com>
2022-09-15 22:52:03 +0900

    prefix

--- 17 누적합/11659.py
+++ 17 누적합/11659.py
@@ -1,14 +1,39 @@
+# import sys
+
+# # prefix 알고리즘을 쓰지 않는다면 시간 복잡도가 O(nm)이 된다
+# # 밑에는 일반 구현
+# input = sys.stdin.readline
+
+# n, m = map(int, input().split())
+
+# lst = list(map(int, input().split()))
+
+# for _ in range(m):
+#     i, j = map(int, input().split())
+#     hap = sum(lst[i-1:j])
+#     print(hap)
+
+# # prefix 알고리즘을 써보자 시간 복잡도 O(n+m)
+
 import sys
 
 input = sys.stdin.readline
 
-x, y = map(int, input().split())
-nums = [0] + list(map(int, input().split()))
+n, m = map(int, input().split())
 
-t = [0] * (x + 1)
-for i in range(1, x + 1):
-    t[i] = t[i - 1] + nums[i]
+lst = list(map(int, input().split()))
 
-for _ in range(y):
-    a, b = map(int, input().split())
-    print(t[b] - t[a - 1])
+# 인덱스를 사용자로부터 받으니 헷갈리지 않기 위해 0을 추가
+prefix_sum = [0]
+
+hap = 0
+
+for i in lst:
+    hap += i
+    prefix_sum.append(hap)
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    print(prefix_sum[j] - prefix_sum[i-1])
+
+
