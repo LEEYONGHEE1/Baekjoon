d48dece3dd1bc0b0541c58dad8be71b6b8ee9842
LEEYONGHEE1 <lyh990104@naver.com>
2022-10-14 23:59:48 +0900

    회의실 배정

--- 18 그리디 알고리즘/1931.py
+++ 18 그리디 알고리즘/1931.py
@@ -10,11 +10,14 @@ count = 0
 
 for i in range(test_number):
     a, b = map(int, input().split())
-    room.append((a,b))
+    room.append([a,b])
+
+room = sorted(room, key=lambda a: a[0])
+room = sorted(room, key=lambda a: a[1])
 
 for i in range(test_number):
-    if(room[i,1] > last_time):
-        last_time = room[i,1]
+    if(room[i][0] >= last_time):
+        last_time = room[i][1]
         count += 1
     
 print(count)
