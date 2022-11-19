8b50487d97e8c0b0c4c4194676b48ece782032d0
LEEYONGHEE1 <lyh990104@naver.com>
2022-11-19 13:51:16 +0900

    랜선 자르기

---
+++ 23 이분 탐색/1654.py
@@ -0,0 +1,17 @@
+import sys
+
+a, b = map(int, input().split())
+len = [int(sys.stdin.readline()) for _ in range(a)]
+start, end = 1, max(len)
+
+while start <= end:
+    mid = (start + end) // 2 
+    line = 0 
+    for i in len:
+        line += i // mid 
+        
+    if line >= b:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
