fcda0a94b9d33e6c1f6dd4e91bd4f33f7ce66ef7
LEEYONGHEE1 <lyh990104@naver.com>
2023-01-15 23:45:25 +0900

    하노이 탑

---
+++ 09 재귀/11729.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+def hanoi(n,a,b,c):
+    if n == 1:
+        print(a,c)
+    else:
+        hanoi(n-1,a,c,b)
+        print(a,c)
+        hanoi(n-1,b,a,c)
+        
+count = 0
+for _ in range(n):
+    count = 2*count + 1
+print(count)
+hanoi(n,1,2,3)
